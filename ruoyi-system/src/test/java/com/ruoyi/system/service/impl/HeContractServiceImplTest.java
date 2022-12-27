5c58f9c77ad8c12eaefc36712062d3369fb07143
AntonyCheng <1911261716@qq.com>
2022-12-27 23:44:22 +0800

    he_contract_V1

---
+++ ruoyi-system/src/test/java/com/ruoyi/system/service/impl/HeContractServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.ruoyi.system.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeContractServiceImplTest {
+
+    public static void main(String[] args) {
+
+    }
+}
